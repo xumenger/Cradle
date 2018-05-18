0ef526c46f0877ee5b323a9b1396aff13b9f05eb
xumenger <xumenger@126.com>
2018-05-18 16:41:33 +0800

    support parse add and subtract

--- src/Cradle.dpr
+++ src/Cradle.dpr
@@ -98,9 +98,39 @@ begin
 end;
 
 { Parse and Translate a Math Expression }
-procedure Expression;
+
+{ Get Term }
+procedure Term;
 begin
   EmitLn('Move #' + GetNum + ',D0');
+end;
+
+{ Recognize and Translate an Add }
+procedure Add;
+begin
+  Match('+');
+  Term;
+  EmitLn('ADD D1, D0');
+end;
+
+{ Recognize and Translate a Subtract }
+procedure Subtract;
+begin
+  Match('-');
+  Term;
+  EmitLn('SUB D1, D0');
+end;
+
+{ BNF: <term> +/- <term> }
+procedure Expression;
+begin
+  Term;
+  EmitLn('Move D0, D1');
+  case Look of
+    '+': Add;
+    '-': Subtract;
+  else Expected('Addop');
+  end;
 end;
 
 {-----------------------Run---------------------------}
